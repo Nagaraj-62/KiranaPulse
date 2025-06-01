0669ffd9f253edc643e8f011d55a44e21184d3e1
Nagaraj <nagarajukodela43@gmail.com>
2025-06-02 00:23:03 +0530

    postgressql Error Fixed.1

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,19 @@
 # app/database.py
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Configure engine based on the DB type
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+else:
+    engine = create_engine(DATABASE_URL, echo=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
